3c63d1b966e1e520cbb451c2b670bf1fba52c6ed
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2025-04-22 20:41:13 -0300

    adicionado função e botão para exclusão da coluna selecionada na tabela

--- main.js
+++ main.js
@@ -26,9 +26,15 @@ function criaLista() {
     let tabela = "<thead><tr><th>Descrição</th><th>Valor</th><th>Categoria</th></tr></thead><tbody>";
 
     for (let i = 0; i < dadosLista.length; i++) {
-        tabela += "<tr><td>" + dadosLista[i].descricao + "</td><td>" + dadosLista[i].valor + "</td><td>" + dadosLista[i].categoria + "</td></tr>";
+        tabela += "<tr><td>" + dadosLista[i].descricao + "</td><td>" + dadosLista[i].valor + "</td><td>" + dadosLista[i].categoria + "</td><td><td><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
     }
 
     tabela += "</tbody>";
     document.getElementById('tabela').innerHTML = tabela;
+}
+
+function excluir(i)
+{
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
 }
